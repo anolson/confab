fbcb48fe8bf09dfb4703a787d2aa0ed91fe014b1
Andrew Olson <anolson@gmail.com>
2016-08-19 10:37:42 -0400

    Refactor.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   has_many :messages
 
   def full_name
-    first_name + ' ' + last_name
+    "#{first_name} #{last_name}"
   end
 
   def self.authenticate(options = {})
